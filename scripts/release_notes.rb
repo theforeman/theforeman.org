649cfe3c91fba9829d3defec931d9d012c420658
Tomer Brisker <tbrisker@gmail.com>
2018-07-23 18:10:27 +0300

    Update release notes script to new redmine releases

--- scripts/release_notes.rb
+++ scripts/release_notes.rb
@@ -10,27 +10,25 @@ require 'json'
 
 URL = 'https://projects.theforeman.org'
 
-# Check out installer modules
-# Compare packages in nightly vs release and retag dependencies
-
-# Create new release via API call
-# Create new search by open + release field
-
 unless ARGV.size == 2
-  puts "Usage: #{$0} PROJECT RELEASE_NAME"
-  puts "Example: #{$0} foreman 1.18.0"
+  puts "Usage: #{$0} PROJECTS RELEASE_NAME"
+  puts "Example: #{$0} foreman,puppet-foreman,selinux,smart-proxy 1.18.0"
   exit 1
 end
 
-@project_name = ARGV[0]
+@project_names = ARGV[0].split(',')
 @current_release_name = ARGV[1]
 
-# Determine version
-def get_version_id
-  url = "#{URL}/projects/#{@project_name}/versions.json"
+def url_to_json(url)
   uri = URI(URI.escape(url))
   response = Net::HTTP.get(uri)
-  result = JSON.parse(response)
+  JSON.parse(response)
+end
+
+# Determine version
+def get_version_id(project)
+  url = "#{URL}/projects/#{project}/versions.json"
+  result = url_to_json(url)
   return nil if result['versions'].nil?
   result = result['versions'].detect{|v| v['name'] == @current_release_name }
   return nil if result.nil?
@@ -40,48 +38,54 @@ rescue
   exit 1
 end
 
-@current_release_id = get_version_id
+@current_release_ids = @project_names.map{|project| get_version_id(project)}.compact
 
-if @current_release_id.nil?
-  puts "Release #{@current_release_name} not found in project #{@project_name}"
+if @current_release_ids.empty?
+  puts "Release #{@current_release_name} not found in any of the projects #{@project_names}"
   exit 1
 end
 
-# Move bugs with Release set to VERSION to NEXT-VERSION with status new
-def gather_issues(offset=0)
-  url = "#{URL}/projects/#{@project_name}/issues.json?status_id=closed&offset=#{offset}&limit=100&fixed_version_id=#{@current_release_id}"
-  uri = URI(URI.escape(url))
-  response = Net::HTTP.get(uri)
-	result = JSON.parse(response)
-	if result['total_count'].to_i - offset - 100 <= 0
-		result['issues']
-	else
-		result['issues'] += gather_issues(offset + 100)
-	end
+def releases_filter
+  @current_release_ids.map{|release| "v[cf_12][]=#{release}"}.join('&')
 end
 
-def modify_target_version!(issue_id, options)
-  uri = URI(URI.escape("#{URL}/issues/#{issue_id}.json"))
-  req = Net::HTTP::Put.new(
-		uri,
-		{ 'Content-Type' => 'application/json',
-		  'X-Redmine-API-Key' => File.read('~/.redminekey') }
-	)
-  Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
+def gather_issues(offset = 0)
+  url = "#{URL}/issues.json?status_id=closed&offset=#{offset}&limit=100&f[]=cf_12&op[cf_12]==&#{releases_filter}"
+  result = url_to_json(url)
+  if result['total_count'].to_i - offset - 100 <= 0
+    result['issues']
+  else
+    result['issues'] += gather_issues(offset + 100)
+  end
 end
+
+
+puts "Copy and paste the following into the release notes section of the relevant manual:"
+
+puts "\n----------[Start of notes]----------\n\n"
+
+puts "### Release notes for #{@current_release_name}"
 
 grouped_issues = gather_issues.group_by { |issue| issue['category']['name'] rescue 'Uncategorized' }
 
 grouped_issues = Hash[ grouped_issues.sort_by { |key, val| key } ]
 grouped_issues.each do |category, issues|
-	puts "#### #{category}"
+  puts "#### #{category}"
 
   issues.each do |issue|
-	  puts "* #{issue['subject']} ([##{issue['id']}](#{URL}/issues/#{issue['id']}))"
+    puts "* #{issue['subject']} ([##{issue['id']}](#{URL}/issues/#{issue['id']}))"
   end
 
   puts
 end
 
-# Close the release
-puts "Close the release #{URL}/versions/#{@current_release_id}/edit - not possible to do through the API (yet)"
+puts "*A full list of changes in {{page.version}} is available via [Redmine](https://projects.theforeman.org/issues?set_filter=1&sort=id%3Adesc&status_id=closed&f[]=cf_12&op[cf_12]=%3D&#{releases_filter})*"
+
+puts "\n----------[End of notes]----------\n"
+
+# Close the releases
+puts "If this isn't an RC release, the relevant releases should be closed (not possible via API):"
+@current_release_ids.each do |release|
+  puts "Close the release #{URL}/versions/#{release}/edit"
+end
+
