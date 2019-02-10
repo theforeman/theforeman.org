6c8347be2b7b2fc5a3b014531eae206a40853223
Jonathon Turel <jturel@gmail.com>
2019-02-10 06:53:55 -0500

    Don't rely on display:none (#1304)

--- static/js/osmenu.js
+++ static/js/osmenu.js
@@ -1,57 +1,59 @@
 $(document).ready(function(){
+    var operatingSystems = {
+        'rhel5': [],
+        'rhel6': ['el6'],
+        'rhel7': ['el7'],
+        'el5': [],
+        'el6': [],
+        'el7': [],
+        'f22': [],
+        'f23': [],
+        'f25': [],
+        'f26': [],
+        'f27': [],
+        'f28': [],
+        'sles11': [],
+        'sles12': []
+    };
+
+    function show(os) {
+        $('#' + os).show();
+        $('.' + os).show();
+    }
+
+    function hide(os) {
+        $('#' + os).hide();
+        $('.' + os).hide();
+    }
+
+    function showOS(operatingSystem) {
+        var os, oses, i = 0;
+
+        for(os in operatingSystems) {
+            if (operatingSystems.hasOwnProperty(os)) {
+                hide(os);
+            }
+        }
+        for(os in operatingSystems) {
+            if (operatingSystems.hasOwnProperty(os)) {
+                if (os === operatingSystem) {
+                    show(os);
+                }
+            }
+        }
+
+        oses = operatingSystems[operatingSystem];
+
+        for(i; i < oses.length; i += 1) {
+            show(oses[i]);
+        }
+
+        return true;
+    }
+
     $('#operatingSystems').change(function() {
-        var operatingSystems = {
-            'rhel5': [],
-            'rhel6': ['el6'],
-            'rhel7': ['el7'],
-            'el5': [],
-            'el6': [],
-            'el7': [],
-            'f22': [],
-            'f23': [],
-            'f25': [],
-            'f26': [],
-            'f27': [],
-            'f28': [],
-            'sles11': [],
-            'sles12': []
-        };
-
-        function show(os) {
-            $('#' + os).show();
-            $('.' + os).show();
-        }
-
-        function hide(os) {
-            $('#' + os).hide();
-            $('.' + os).hide();
-        }
-
-        function showOS(operatingSystem) {
-            var os, oses, i = 0;
-
-            for(os in operatingSystems) {
-                if (operatingSystems.hasOwnProperty(os)) {
-                    hide(os);
-                }
-            }
-            for(os in operatingSystems) {
-                if (operatingSystems.hasOwnProperty(os)) {
-                    if (os === operatingSystem) {
-                        show(os);
-                    }
-                }
-            }
-
-            oses = operatingSystems[operatingSystem];
-
-            for(i; i < oses.length; i += 1) {
-                show(oses[i]);
-            }
-
-            return true;
-        }
-
         showOS($(this).val());
     });
+
+    showOS($('#operatingSystems').val());
 });
