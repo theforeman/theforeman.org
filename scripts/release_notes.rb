f95448f8f3df084fedbf25672fe734a4f7b2ed89
Tomer Brisker <tbrisker@users.noreply.github.com>
2019-11-26 16:38:52 +0200

    Update release notes for 1.24-RC3 (#1507)

--- scripts/release_notes.rb
+++ scripts/release_notes.rb
@@ -68,6 +68,8 @@ puts "### Release notes for #{@current_release_name}"
 
 grouped_issues = gather_issues.group_by { |issue| issue['category']['name'] rescue issue['project']['name'] }
 
+grouped_issues['Smart Proxy'] += grouped_issues.delete('Core')
+grouped_issues['SELinux'] += grouped_issues.delete('General Foreman')
 grouped_issues = Hash[ grouped_issues.sort_by { |key, val| key } ]
 grouped_issues.each do |category, issues|
   puts "#### #{category}"
