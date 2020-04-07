d7f7cd592911d107b5144425ce6d731ebfc016ca
Tomer Brisker <tbrisker@gmail.com>
2020-04-07 19:04:00 +0300

    Update contributors for 2.0

--- scripts/committers.rb
+++ scripts/committers.rb
@@ -3,8 +3,8 @@ require 'tmpdir'
 require 'git'
 
 @tagged_repos = %w(foreman smart-proxy foreman-installer foreman-selinux)
-@tag_from     = "1.23.0"
-@tag_to       = "1.24.0"
+@tag_from     = "1.24.0"
+@tag_to       = "2.0.0"
 
 @untagged_repos = %w(
   theforeman.org
@@ -36,8 +36,8 @@ require 'git'
   ovirt-node-plugin-foreman
   puppetdb_foreman
 )
-@date_from     = '2019-09-10'
-@date_to       = '2019-12-10'
+@date_from     = '2019-12-11'
+@date_to       = '2020-04-06'
 
 @author_map = {
   "abenari"        => "Amos Benari",
@@ -108,7 +108,10 @@ require 'git'
   "sthirugn" => "Sureshkumar Thirugnanasambandan",
   "swetha" => "Swetha Seelam Lakshmi Narayanan",
   "jkalchik" => "Jeff Kalchik",
-
+  "fschaer" => "Frederic Schaer",
+  "ksagarwal007" => "Kaushik Agarwal",
+  "neilfromit" => "Neil Townsend",
+  "William Bradford Clark" => "William Clark",
 }
 
 @authors={}
